d311032556666e259ab6138fac3ac72f6fdf8374
Mikhail1Solovev <163019759+Mikhail1Solovev@users.noreply.github.com>
2024-06-24 15:27:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-hostName = "localhost"
-serverPort = 8080
+host_name = "localhost"
+server_port = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        with open("templates/index.html", "r", encoding="utf-8") as file:
+        with open("index.html", "r", encoding="utf-8") as file:
             self.wfile.write(bytes(file.read(), "utf-8"))
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print(f"Server started http://{hostName}:{serverPort}")
+    web_server = HTTPServer((host_name, server_port), MyServer)
+    print(f"Server started http://{host_name}:{server_port}")
 
     try:
-        webServer.serve_forever()
+        web_server.serve_forever()
     except KeyboardInterrupt:
         pass
 
-    webServer.server_close()
+    web_server.server_close()
     print("Server stopped.")
